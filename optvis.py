92fa1833fbc52fda85c7be7718b06d9bf581a72b
elichen <eli.chen@gmail.com>
2019-12-22 00:40:43 +0000

    remove non-fft/normal colorspace options

--- optvis.py
+++ optvis.py
@@ -3,6 +3,7 @@ import torch
 from torch import tensor
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
+from torchvision import transforms
 
 def init_fft_buf(size):
     sd = 0.01
@@ -26,12 +27,7 @@ def fft_to_rgb(t, d=0.5, decay_power=1, fft_magic=4.0, **kwargs):
 
     return image_t
 
-def init_pixel_buf(size):
-    img_buf = torch.empty(1,3,size,size).normal_(mean=0,std=0.01)
-    img_buf = torch.sigmoid(img_buf).cuda()
-    return img_buf
-    
-def lucid_to_rgb(t):
+def lucid_colorspace_to_rgb(t):
     color_correlation_svd_sqrt = np.asarray([[0.26, 0.09, 0.02],
                                              [0.27, 0.00, -0.05],
                                              [0.27, -0.09, 0.03]]).astype(np.float32)
@@ -43,12 +39,12 @@ def lucid_to_rgb(t):
     t = t_flat.permute(0,3,1,2)
     return t
 
-def image_buf_to_rgb(img_buf, jitter, decorrelate=True, fft=True, **kwargs):
+def image_buf_to_rgb(img_buf, jitter, **kwargs):
     img = img_buf.detach()
-    if fft: img=fft_to_rgb(img, **kwargs)
+    img = fft_to_rgb(img, **kwargs)
     size = img.shape[-1]
     x_off,y_off = jitter//2,jitter//2
-    if decorrelate: img = lucid_to_rgb(img)
+    img = lucid_colorspace_to_rgb(img)
     img = torch.sigmoid(img)
     img = img[:,:,x_off:x_off+size-jitter,y_off:y_off+size-jitter] # jitter
     img = img.squeeze()    
@@ -66,9 +62,8 @@ def show_rgb(img, label=None, ax=None, dpi=25, **kwargs):
 def visualize_feature(model, layer, feature,
                       size=200, jitter=25,
                       steps=2000, lr=0.05,
-                      decorrelate=True, fft=True,
                       debug=False, frames=10, show=True, **kwargs):
-    img_buf = init_fft_buf(size+jitter) if fft else init_pixel_buf(size+jitter)
+    img_buf = init_fft_buf(size+jitter)
     img_buf.requires_grad_()
     opt = torch.optim.Adam([img_buf], lr=lr)
 
@@ -80,9 +75,9 @@ def visualize_feature(model, layer, feature,
     
     for i in range(1,steps+1):
         x_off, y_off = int(np.random.random()*jitter),int(np.random.random()*jitter)
-        img = fft_to_rgb(img_buf, **kwargs) if fft else img_buf
+        img = fft_to_rgb(img_buf, **kwargs)
         img = img[:,:,x_off:x_off+size+1,y_off:y_off+size+1] # jitter
-        if decorrelate: img = lucid_to_rgb(img) # decorrelate color
+        img = lucid_colorspace_to_rgb(img)
         model(img.cuda())
         opt.zero_grad()
         loss = -1*hook_out[0][feature].mean()
@@ -90,12 +85,12 @@ def visualize_feature(model, layer, feature,
         opt.step()
         if debug and (i)%(steps/frames)==0:
             clear_output(wait=True)
-            show_rgb(image_buf_to_rgb(img_buf, jitter, decorrelate=decorrelate, fft=fft, **kwargs),
+            show_rgb(image_buf_to_rgb(img_buf, jitter, **kwargs),
                      label=f"step: {i} loss: {loss}", **kwargs)
 
     hook.remove()
     
-    retval = image_buf_to_rgb(img_buf, jitter, decorrelate=decorrelate, fft=fft, **kwargs)
+    retval = image_buf_to_rgb(img_buf, jitter, **kwargs)
     if show:
         if not debug: show_rgb(retval, **kwargs)
     else:
